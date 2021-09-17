526a6991f9909be57c39b15967223ea028d91287
veroanggra <veronicaputri497@gmail.com>
2021-09-17 11:38:57 +0700

    initial

--- app/src/main/java/com/veronica/idn/foodiest/MainActivity.kt
+++ app/src/main/java/com/veronica/idn/foodiest/MainActivity.kt
@@ -2,11 +2,37 @@ package com.veronica.idn.foodiest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.veronica.idn.foodiest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    /*
+    modifier : private, protected, inner
+    val  -> value : immutable
+    var -> bisa diubah2 : muttable*/
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
         supportActionBar?.hide()
+        setCurrentFragment(BreakfastFragment())
+        mainBinding.navBottomMain.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.ic_breakfast -> setCurrentFragment(BreakfastFragment())
+                R.id.ic_vegetarian -> setCurrentFragment(VegetarianFragment())
+                R.id.ic_dessert -> setCurrentFragment(DessertFragment())
+            }
+            true
+        }
     }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_main, fragment)
+            commit()
+        }
+
 }
